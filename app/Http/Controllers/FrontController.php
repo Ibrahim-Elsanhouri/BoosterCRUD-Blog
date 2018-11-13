3c6de30cdc48178e4e7bc81005d224d979cd56f2
Ibrahim-Elsanhouri <ibrahimelsanhouri@gmail.com>
2018-11-13 16:06:17 +0300

    Booster CRUD Package Blog

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+class FrontController extends Controller
+{
+    //
+    public $blog_name = 'My Simple Blog' ; 
+    public function getIndex(){
+        $data['page_title'] = 'Home Page';
+        $data['page_description'] = 'This is my simple blog'; 
+        $data['blog_name'] = $this->blog_name; 
+        $data['categories']= DB::table('categories')->get(); 
+        $data['result'] = DB::table('posts')->
+        join('categories' , 'categories.id' , '=' , 'category_id')->
+        join('cms_users' , 'cms_users.id' , '=' , 'cms_users_id')->
+        select('posts.*', 'categories.name as name_categories' , 'cms_users.name as name_authors')
+        ->orderby('posts.id' , 'desc')->take(5)->get();
+        return view ('index', $data); 
+
+        ; 
+    }
+    public function getArticle($slug){
+        $data['row'] = DB::table('posts')->
+        join('categories' , 'categories.id' , '=' , 'category_id')->
+        join('cms_users' , 'cms_users.id' , '=' , 'cms_users_id')->
+        select('posts.*', 'categories.name as name_categories' , 'cms_users.name as name_authors')
+        ->where('posts.slug' , $slug)->first();
+        $data['categories']= DB::table('categories')->get();
+        return view ('details', $data); 
+    
+}
+public function getByCategories($id){
+    $data['rows'] = DB::table('posts')->
+    join('categories' , 'categories.id' , '=' , 'category_id')->
+    join('cms_users' , 'cms_users.id' , '=' , 'cms_users_id')->
+    select('posts.*', 'categories.name as name_categories' , 'cms_users.name as name_authors')
+    ->where('posts.category_id' , $id)->paginate(5);
+    $data['categories']= DB::table('categories')->get();
+    return view ('cat', $data); 
+}
+public function getLatest(){
+    $data['row'] = DB::table('posts')->
+    join('categories' , 'categories.id' , '=' , 'category_id')->
+    join('cms_users' , 'cms_users.id' , '=' , 'cms_users_id')->
+    select('posts.*', 'categories.name as name_categories' , 'cms_users.name as name_authors')
+    ->orderby('posts.id' , 'desc')->paginate(2);
+    $data['categories']= DB::table('categories')->get();
+    return view ('latest', $data); 
+}
+public function getSearch(Request $request){
+    if ($request->q =='') return redirect('/');
+    $data['row'] = DB::table('posts')->
+    join('categories' , 'categories.id' , '=' , 'category_id')->
+    join('cms_users' , 'cms_users.id' , '=' , 'cms_users_id')->
+    select('posts.*', 'categories.name as name_categories' , 'cms_users.name as name_authors')
+    ->where('posts.title' ,'like' ,'%' . $request->q . '%')->paginate(2);
+    $data['categories']= DB::table('categories')->get();
+    return view ('latest', $data); 
+}
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CRUDBooster Blog</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-post.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+      @foreach($result as $row)
+        <!-- Post Content Column -->
+     
+        <div class="col-lg-8">
+
+          <!-- Title -->
+          <h1 class="mt-4">{{$row->title}}</h1>
+
+          <!-- Author -->
+          <p class="lead">
+            by
+            <a href="#">{{ $row->name_authors }}</a>
+          </p>
+
+          <hr>
+
+          <!-- Date/Time -->
+          <p>Posted on {{ date('M,d Y' , strtotime($row->created_at))}}</p>
+
+          <hr>
+
+         
+
+      
+<p>{{ str_limit (strip_tags($row->content), 200) }}</p>
+
+      
+          <hr>
+         <p><a href="/article/{{ $row->slug }}">Read More</a></p>
+      </div>
+      @endforeach
+          <!-- Comment with nested comments -->
+        
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-md-4">
+
+          <!-- Search Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Search</h5>
+            <div class="card-body">
+              <div class="input-group">
+              <form action="/search"   method="get">
+                <input type="text" name="q" class="form-control" placeholder="Search for...">
+               
+                  <button type="submit" class="btn btn-secondary" type="button">Go!</button>
+              
+
+              </div>
+            </div>
+          </div>
+
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Categories</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul class="list-unstyled mb-0">
+                  @foreach($categories as $category)
+                    <li>
+                      <a href="/cat/{{$category->id}}">{{ $category->name }}</a>
+                    </li>
+               @endforeach
+                  </ul>
+                </div>
+        
+              </div>
+            </div>
+          </div>
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Side Widget</h5>
+            <div class="card-body">
+              You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
